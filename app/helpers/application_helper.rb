9db743d77c34676a395a7da4ac7e790c7d7c462a
Yarek T <yarek@delta.lan>
2010-12-06 23:13:24 +0000

    - New design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,26 @@
 module ApplicationHelper
 	def state(state_id)
 		case state_id
-			when Virt::NO_STATE 		then state = "No State"
-			when Virt::RUNNING 			then state = "Running"
-			when Virt::BLOCKED 			then state = "Blocked"
-			when Virt::SUSPENDED 		then state = "Suspended"
-			when Virt::SHUTTING_DOWN 	then state = "Shut Down"
-			when Virt::OFF 				then state = "Shut Off"
-			when Virt::CRASHED 			then state = "Crashed"
+			when Virt::NO_STATE 		
+				then return {:name => "No State", :class => 'NoState'}
+			when Virt::RUNNING 			
+				then return {:name => "Running", :class => 'Running'}
+			when Virt::BLOCKED 			
+				then return {:name => "Blocked", :class => 'Blocked'}
+			when Virt::SUSPENDED 		
+				then return {:name => "Suspended", :class => 'Suspended'}
+			when Virt::SHUTTING_DOWN 	
+				then return {:name => "Shut Down", :class => 'ShuttingDown'}
+			when Virt::OFF 				
+				then return {:name => "Shut Off", :class => 'Off'}
+			when Virt::CRASHED 			
+				then return {:name => "Crashed", :class => 'Crashed'}
 		end
-		
-		return state
 	end
 	
 	def capabilities(status)
 		cap = Array.new
 		# Display Start (Create) if the VM is Virt::OFF
-		if status == Virt::OFF
-			cap << { :name => 'Start',    :command => :virtStart }
-		else # Display Destroy if domain is anything but Virt::OFF
-			cap << { :name => 'Destroy',  :command => :virtStop }
-		end
 		if status == Virt::RUNNING
 			cap << { :name => 'Suspend',  :command => :virtSuspend }
 			cap << { :name => 'Shutdown', :command => :noop }
@@ -28,6 +28,11 @@ module ApplicationHelper
 		end
 		if status == Virt::SUSPENDED
 			cap << { :name => 'Resume',   :command => :virtResume }
+		end
+		if status == Virt::OFF
+			cap << { :name => 'Start',    :command => :virtStart }
+		else # Display Destroy if domain is anything but Virt::OFF
+			cap << { :name => 'Destroy',  :command => :virtStop }
 		end
 		return cap
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Rubypanel::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "dash#index"
 
   # See how all your routes lay out with "rake routes"
 
